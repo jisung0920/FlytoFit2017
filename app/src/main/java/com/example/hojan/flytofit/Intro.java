6486be18de4d27188ccd42723c96bc78c64382b6
December15 <hojanrgid@naver.com>
2017-07-12 15:57:32 +0900

    첫번째 commit

---
+++ app/src/main/java/com/example/hojan/flytofit/Intro.java
@@ -0,0 +1,23 @@
+package com.example.hojan.flytofit;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(Intro.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+}}
